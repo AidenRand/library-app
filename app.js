0c3edd2cac59a1a0807996b1eceb88b3d4deafe8
Cortrax <aidenrphotos@gmail.com>
2022-09-13 10:23:49 -0400

    Add read button

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const authorIn = document.querySelector(".authorInput");
 const titleIn = document.querySelector(".bookNameInput");
 const pagesIn = document.querySelector(".pagesInput");
-const readIn = document.querySelector(".readInput");
+const readBtn = document.querySelector(".readBtn");
 const btn1 = document.querySelector(".btn1");
 let bookContainer = document.querySelector(".bookContainer");
 let inputContainer = document.querySelector(".input");
 let newBookBtn = document.querySelector(".newBookBtn");
+let bookCounter = document.querySelector(".bookCounter");
+let deleteBtn = document.getElementById("closeBtn");
 
 let myLibrary = [];
 
@@ -13,14 +15,16 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 function userInput() {
   let title = titleIn.value;
   let author = authorIn.value;
   let pages = pagesIn.value;
+  let read = readBtn.checked;
 
-  let newBook = new Book(title, author, pages);
+  let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
@@ -31,23 +35,52 @@ function displayBooks() {
     let authorT = myLibrary[i].author;
     let titleT = myLibrary[i].title;
     let pagesT = myLibrary[i].pages;
+    let readT = myLibrary[i].read;
 
     let bookDiv = document.createElement("div");
     let authorPara = document.createElement("p");
     let titlePara = document.createElement("p");
     let pagesPara = document.createElement("p");
+    let closeBtn = document.createElement("button");
+    let readButton = document.createElement("button");
+    readButton.classList += "readButton";
     bookDiv.classList += "bookDiv";
     authorPara.classList += "author";
     titlePara.classList += "title";
     pagesPara.classList += "pages";
-    authorPara.innerHTML = `author: ${authorT}`;
-    titlePara.innerHTML = `title: ${titleT}`;
-    pagesPara.innerHTML = `pages: ${pagesT}`;
+    closeBtn.classList += "closeBtn";
+    authorPara.innerHTML = authorT;
+    titlePara.innerHTML = titleT;
+    pagesPara.innerHTML = `Pages: ${pagesT}`;
+    closeBtn.innerHTML = "Delete";
+    readButton.innerHTML = readT;
 
+    bookDiv.appendChild(closeBtn);
+    bookDiv.appendChild(readButton);
+    bookDiv.appendChild(pagesPara);
     bookDiv.appendChild(authorPara);
     bookDiv.appendChild(titlePara);
-    bookDiv.appendChild(pagesPara);
     bookContainer.appendChild(bookDiv);
+
+    readButton.addEventListener("click", () => {
+      if (readButton.textContent === 'Read') {
+        readButton.style.backgroundColor = "#F44A26";
+        readButton.innerHTML = "Not Read";
+      } else {
+        readButton.innerHTML = "Read";
+        readButton.style.backgroundColor = "#54B82B";
+      }
+    })
+
+    if (readT) {
+      readButton.innerHTML = "Read";
+      readButton.style.backgroundColor = "#54B82B";
+    } else {
+      readButton.style.backgroundColor = "#F44A26";
+      readButton.innerHTML = "Not Read";
+    }
+
+    closeBtn.addEventListener("click", () => {});
   }
 }
 
@@ -57,15 +90,21 @@ function resetForm() {
   pagesIn.value = "";
 }
 
+
+function updateBookCounter() {
+  bookCounter.textContent = `Books: ${myLibrary.length}`;
+}
+
 btn1.addEventListener("click", () => {
   userInput();
   displayBooks();
   resetForm();
+  updateBookCounter();
   inputContainer.style.opacity = 0;
   inputContainer.style.pointerEvents = "none";
 });
 
-newBookBtn.addEventListener('click', () => {
+newBookBtn.addEventListener("click", () => {
   inputContainer.style.opacity = 1;
   inputContainer.style.pointerEvents = "all";
-})
+});
