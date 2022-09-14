9292318a7f3dc7bae1b6d6717e793eeb8ab0fc4d
Cortrax <aidenrphotos@gmail.com>
2022-09-14 11:24:48 -0400

    Add css styling

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ function userInput() {
 
 function displayBooks() {
   bookContainer.innerHTML = "";
+  storeLocal();
 
   for (let i = 0; i < myLibrary.length; i++) {
     let authorT = myLibrary[i].author;
@@ -41,21 +42,23 @@ function displayBooks() {
     let authorPara = document.createElement("p");
     let titlePara = document.createElement("p");
     let pagesPara = document.createElement("p");
-    let closeBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
     let readButton = document.createElement("button");
+
     readButton.classList += "readButton";
     bookDiv.classList += "bookDiv";
     authorPara.classList += "author";
     titlePara.classList += "title";
     pagesPara.classList += "pages";
-    closeBtn.classList += "closeBtn";
+    deleteBtn.classList += "deleteBtn";
+
     authorPara.innerHTML = authorT;
     titlePara.innerHTML = titleT;
     pagesPara.innerHTML = `Pages: ${pagesT}`;
-    closeBtn.innerHTML = "Delete";
+    deleteBtn.innerHTML = "<i class='fa-solid fa-xmark'></i>";
     readButton.innerHTML = readT;
 
-    bookDiv.appendChild(closeBtn);
+    bookDiv.appendChild(deleteBtn);
     bookDiv.appendChild(readButton);
     bookDiv.appendChild(pagesPara);
     bookDiv.appendChild(authorPara);
@@ -67,20 +70,31 @@ function displayBooks() {
         myLibrary[i].read = false;
         readButton.style.backgroundColor = "#F44A26";
         readButton.textContent = "Not Read";
+        storeLocal();
       } else {
         readButton.textContent = "Read";
         readButton.style.backgroundColor = "#54B82B";
         myLibrary[i].read = true;
+        storeLocal();
       }
     });
 
     if (myLibrary[i].read) {
       readButton.textContent = "Read";
       readButton.style.backgroundColor = "#54B82B";
+      storeLocal();
     } else {
       readButton.style.backgroundColor = "#F44A26";
       readButton.textContent = "Not Read";
+      storeLocal();
     }
+
+    deleteBtn.addEventListener("click", () => {
+      bookContainer.removeChild(bookDiv);
+      myLibrary.splice(bookDiv, 1);
+      storeLocal();
+      updateBookCounter();
+    });
   }
 }
 
@@ -102,9 +116,28 @@ btn1.addEventListener("click", () => {
   updateBookCounter();
   inputContainer.style.opacity = 0;
   inputContainer.style.pointerEvents = "none";
+  storeLocal();
 });
 
 newBookBtn.addEventListener("click", () => {
   inputContainer.style.opacity = 1;
   inputContainer.style.pointerEvents = "all";
 });
+
+function storeLocal() {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function getLocalStorage() {
+  if (!localStorage.myLibrary) {
+    displayBooks();
+    updateBookCounter();
+  } else {
+    let items = localStorage.getItem("myLibrary");
+    items = JSON.parse(items);
+    myLibrary = items;
+    displayBooks();
+    updateBookCounter();
+  }
+}
+getLocalStorage();
